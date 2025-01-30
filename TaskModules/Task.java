a62455c0b9c587978033274a58a4cdad33fcc7a9
your full name here <gserwaa998@gmail.com>
2025-01-30 10:07:12 +0000

    Add model class

---
+++ TaskModules/Task.java
@@ -0,0 +1,42 @@
+package TaskModules;
+
+public class Task {
+    private long id;
+    private String description;
+    private String status;
+
+    public Task(long id, String description, String status) {
+        this.id = id;
+        this.description = description;
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return id + ". " + description + " [" + status + "]";
+    }
+}
